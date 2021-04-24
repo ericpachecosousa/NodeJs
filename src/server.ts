e8f3ec323dee9bfb2d6e48bfd94687dca505bfef
ericpachecosousa <ericpachecos@gmail.com>
2021-04-24 14:32:45 -0300

    estudo 24/04

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import {createServer} from "http";
+import { Server,Socket } from "socket.io"
 import './database';
 import { routes } from "./routes";
 
 const app = express();
+
+const http = createServer(app);//criando o protocolo http
+
+const io = new Server(http); //criando protocolo wss
+
+io.on("connection", (socket:Socket)=>{
+    console.log("Se conectou",socket.id);
+)};
 
 app.use(express.json())
 
