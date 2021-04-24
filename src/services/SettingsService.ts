3ba436bddb47f4d94b128216adef60217d4668f1
ericpachecosousa <ericpachecos@gmail.com>
2021-04-24 14:32:55 -0300

    estudo

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate{
+  chat:boolean;
+  username:string;
+}
+
+class SettingsService {
+  private settingsRepository:Repository<Setting>;
+
+  constructor(){
+    this.settingsRepository = getCustomRepository(SettingsRepository)
+  }
+  
+  async create({chat,username}:ISettingsCreate){
+
+    const UserAlreadyExists = await this.settingsRepository.findOne({
+      username
+    });
+
+    if(UserAlreadyExists)
+    {
+      throw new Error("User already exists!");
+    }
+
+    const settings = this.settingsRepository.create({
+      chat,
+      username,
+    });
+    await this.settingsRepository.save(settings);
+    return settings;
+  }
+}
+export {SettingsService}
